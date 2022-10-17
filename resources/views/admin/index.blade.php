a8da1a245d9dbaa09f3ef24083f47887854451dc
artemnikulin <itsacats@gmail.com>
2022-10-17 16:29:38 +0300

    add blade for admin

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.main')
+
+@section('title')
+    @parent Админка
+@endsection
+
+@section('menu')
+    @include('admin.menu')
+@endsection
+
+@section('content')
+    <div>
+        <h3>Админка</h3>
+    </div>
+@endsection
+
---
+++ resources/views/admin/test1.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.main')
+
+@section('title')
+    @parent Админка
+@endsection
+
+@section('menu')
+    @include('admin.menu')
+@endsection
+
+@section('content')
+    <div>
+        <h4>Тест 1</h4>
+    </div>
+@endsection
